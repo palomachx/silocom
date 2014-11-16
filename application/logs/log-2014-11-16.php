e4ff37171af07ecb954d670b36641a9ac3ba2cc3
Paloma <palomachx@gmail.com>
2014-11-16 16:40:17 -0600

    Se creo repositorio de proyecto silocom

---
+++ application/logs/log-2014-11-16.php
@@ -0,0 +1,64 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2014-11-16 23:03:43 --> Config Class Initialized
+DEBUG - 2014-11-16 23:03:43 --> Hooks Class Initialized
+DEBUG - 2014-11-16 23:03:43 --> Utf8 Class Initialized
+DEBUG - 2014-11-16 23:03:43 --> UTF-8 Support Enabled
+DEBUG - 2014-11-16 23:03:43 --> URI Class Initialized
+DEBUG - 2014-11-16 23:03:43 --> Router Class Initialized
+DEBUG - 2014-11-16 23:03:43 --> No URI present. Default controller set.
+DEBUG - 2014-11-16 23:03:43 --> Output Class Initialized
+DEBUG - 2014-11-16 23:03:43 --> Security Class Initialized
+DEBUG - 2014-11-16 23:03:43 --> Input Class Initialized
+DEBUG - 2014-11-16 23:03:43 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-11-16 23:03:43 --> Language Class Initialized
+DEBUG - 2014-11-16 23:03:43 --> Loader Class Initialized
+DEBUG - 2014-11-16 23:03:43 --> Database Driver Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> Config Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> Hooks Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> Utf8 Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> UTF-8 Support Enabled
+DEBUG - 2014-11-16 23:05:02 --> URI Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> Router Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> No URI present. Default controller set.
+DEBUG - 2014-11-16 23:05:02 --> Output Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> Security Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> Input Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-11-16 23:05:02 --> Language Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> Loader Class Initialized
+DEBUG - 2014-11-16 23:05:02 --> Database Driver Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> Config Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> Hooks Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> Utf8 Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> UTF-8 Support Enabled
+DEBUG - 2014-11-16 23:05:25 --> URI Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> Router Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> No URI present. Default controller set.
+DEBUG - 2014-11-16 23:05:25 --> Output Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> Security Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> Input Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-11-16 23:05:25 --> Language Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> Loader Class Initialized
+DEBUG - 2014-11-16 23:05:25 --> Helper loaded: url_helper
+DEBUG - 2014-11-16 23:05:25 --> Database Driver Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> Config Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> Hooks Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> Utf8 Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> UTF-8 Support Enabled
+DEBUG - 2014-11-16 23:08:15 --> URI Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> Router Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> No URI present. Default controller set.
+DEBUG - 2014-11-16 23:08:15 --> Output Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> Security Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> Input Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> Global POST and COOKIE data sanitized
+DEBUG - 2014-11-16 23:08:15 --> Language Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> Loader Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> Helper loaded: url_helper
+DEBUG - 2014-11-16 23:08:15 --> Database Driver Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> Controller Class Initialized
+DEBUG - 2014-11-16 23:08:15 --> File loaded: application/views/welcome_message.php
+DEBUG - 2014-11-16 23:08:15 --> Final output sent to browser
+DEBUG - 2014-11-16 23:08:15 --> Total execution time: 0.0900
