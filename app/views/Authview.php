e2570d7eb4908b0c3969edfd1d29a95716bec1d8
coderdiaz <javier.diaz.internaut@gmail.com>
2015-02-21 19:16:22 -0600

    Nuevo estilo para el inicio de sesión

--- app/views/Authview.php
+++ app/views/Authview.php
@@ -4,27 +4,20 @@
 	<div id="logtemplate">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-4 col-lg-offset-4">
-					<div class="login-panel">
-						<div class="logotype">
-							<img src="public/img/logotype.png" width="80%" />
-						</div>
-						<div class="form">
-							<form>
-								<div class="form-group">
-									<input type="text" class="form-control" name="username" placeholder="Nombre de Usuario" />
-								</div>
-								<div class="form-group">
-									<input type="password" class="form-control" name="passwd" placeholder="Contraseña" />
-								</div>
-								<div class="form-group">
-									<button class="btn btn-success col-lg-12" type="submit">Login</button>
-								</div>
-							</form>
-						</div>
-						<div class="footpanel text-center">
-							<span >&copy; Silocom @ Development </span>
-						</div>
+				<div class="col-lg-6">
+					<img src="public/img/logotype.png" class="right" width="410" />
+				</div>
+				<div class="col-lg-6">
+					<div class="panel-login">
+						<form>
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Nombre de Usuario" />
+							</div>
+							<div class="form-group">
+								<input type="password" class="form-control" placeholder="Contraseña" />
+							</div>
+							<button type="submit" class="btn-submit btn btn-primary btn-clean-pink right">Acceder a la Plataforma</button>
+						</form>
 					</div>
 				</div>
 			</div>
