421c6bd14f7bf8bca8a955b4eb7b99d569fa9e3f
Javier Diaz <coderdiaz@gmail.com>
2015-04-24 01:09:17 -0500

    Agregados nuevos estilos para las playlists

--- app/views/templates/list-playlist.php
+++ app/views/templates/list-playlist.php
@@ -4,19 +4,30 @@
       <div class="">
         <br />
         <ul class="nav navbar-nav">
+          <p class="text-center label-top"><span>Playlists de <?=$lista->username;?></span></p>
           <li class="li-title-nav">
             <!-- <?php var_dump($lista); ?> -->
-            <p class="text-center"><span>Playlists de <?=$lista->username;?></span></p>
             <?php if($lista->data != false): ?>
             <?php else: ?>
-              <p class="no-data">
+              <!-- <p class="no-data">
                 <b style="font-size: 70px;margin-top: 50px;"><span class="glyphicon glyphicon-headphones"></span></b>
                 <br/>
                 No se encontraron playlist disponibles.
-              </p>
+              </p> -->
               <div class="button-playlist">
                 <a href="#" class="btn btn-outline btn-outline-static col-lg-12">Crear una Playlist</a>
               </div>
+              <ul id="l_playlist" class="nav nav-pills nav-stacked nav-playlist">
+                <li>
+                  <a class="atem" href="#"><span class="glyphicon glyphicon-music icons-playlist"></span> Playlist Name</a>
+                </li>
+                <li>
+                  <a class="atem" href="#"><span class="glyphicon glyphicon-music icons-playlist"></span> Playlist Name</a>
+                </li>
+                <li>
+                  <a class="atem" href="#"><span class="glyphicon glyphicon-music icons-playlist"></span> Playlist Name</a>
+                </li>
+              </ul>
             <?php endif; ?>
           </li>
         </ul>
